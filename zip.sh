3600bb1e04b179e6701a9d798e777b0ed46bd7f7
AAMIRR_ALI <amirwide@gmail.com>
2024-12-25 23:41:44 +0530

    defconfig: AAliZ->YazzdaN

--- zip.sh
+++ zip.sh
@@ -2,6 +2,6 @@ cp out/arch/arm64/boot/Image ./anykernel/
 cp out/arch/arm64/boot/dtbo.img ./anykernel/
 
 
-cd anykernel && zip -r AAliZ-kernel-AOSP-$(date +"%d-%m-%Y"-%H%M).zip * && mv AAliZ-kernel-AOSP-$(date +"%d-%m-%Y-%H%M").zip ../out/
+cd anykernel && zip -r Y_zzdan-kernel-AOSP-$(date +"%d-%m-%Y"-%H%M).zip * && mv Y_zzdan-kernel-AOSP-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
 cd ..
